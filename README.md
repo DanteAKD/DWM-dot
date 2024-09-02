5e87e2d3b2080238ba7be36216750801997e1a85
DanteAKD <marino.kari24@gmail.com>
2024-09-02 20:15:50 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Install dependencies: ueberzugpp, clipmenu, picom-ftlabs-git, zen-browser, ranger, flameshot, ly, lollypop, visual-studio-code-bin, fcitx5 flatpak, thunar, mpv, ani-cli, obs, sof-firmware.
---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    fastfetch
+    fish_add_path .local/bin
+    fish_add_path .cargo/bin
+
+    alias xs="sudo pacman -S"
+    alias xr="sudo pacman -Rs"
+    alias xu="sudo pacman -Suy"
+
+    alias pwd="ptpwd"
+    alias cp="ptcp"
+
+    alias e="nvim"
+    alias se="sudo -E -s nvim"
+end
